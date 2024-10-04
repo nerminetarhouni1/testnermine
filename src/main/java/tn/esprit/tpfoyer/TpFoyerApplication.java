12aecd3a16beda6e901d552308335a28ea32e1b9
malekturky <mohamedmalek.turki@esprit.tn>
2024-10-04 23:26:25 +0200

    ajouter un commentaire dans un fichier dans src du projet tpfoyer-devops mais j'ai oublier

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -1,6 +1,6 @@
 package tn.esprit.tpfoyer;
 
-
+//comment added
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
