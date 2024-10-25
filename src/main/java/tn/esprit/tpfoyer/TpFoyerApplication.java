bd8fc9d8fc73c9305b44855bbe6e80746329e9aa
nermine tarhouni <124258253+nerminetarhouni1@users.noreply.github.com>
2024-10-26 01:40:54 +0200

    Update TpFoyerApplication.java

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -1,17 +1,23 @@
 package tn.esprit.tpfoyer;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
 public class TpFoyerApplication {
+     @GetMapping("/")
+    public String getmessage()
+    {
+        return "good job nermine your java app is deployed with docker successfully ";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TpFoyerApplication.class, args);
