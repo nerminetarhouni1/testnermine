96e793dd129ac6203f5bdf766d2cecf5506ba250
malekturky <mohamedmalek.turki@esprit.tn>
2024-10-25 19:38:48 +0200

    new commit dockersrc

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -6,14 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //bleda
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
+@RestController
 public class TpFoyerApplication {
-
+    @GetMapping("/")
+    public String getmessage()
+    {
+        return "good job mohmed malek turki your java app is deployed with docker successfully ";
+    }
     public static void main(String[] args) {
+ 
         SpringApplication.run(TpFoyerApplication.class, args);
     }
 
