565c9bdb723c1fdeedac4e7a7655e5886502e666
nermine tarhouni <124258253+nerminetarhouni1@users.noreply.github.com>
2024-10-27 22:46:12 +0100

    Update EtudiantRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/etudiant")
+@CrossOrigin(origins = "*") 
 public class EtudiantRestController {
 
     IEtudiantService etudiantService;
