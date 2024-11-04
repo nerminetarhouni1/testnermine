adb3849a3ee0e1ac19a205b761b1674e60aa9efe
ChehataMedAziz <chehata.medaziz@esprit.tn>
2024-11-04 09:32:03 +0000

    Updated

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -6,12 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
 public class TpFoyerApplication {
+
+     @GetMapping("/")
+    public String getmessage()
+    {
+        return "docker successfully ";
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(TpFoyerApplication.class, args);
